ae4af29a380a0c60faa5d239f898510d60413791
simonzz <kczhang@gmail.com>
2013-01-02 21:57:20 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3test::Application.config.secret_token = '1d6107f3b30c91bc254dece0b5b7365a1a137dd825cefd95a8a9187e0b91550f05b2c489e3b7466d5fe4ad11287d4a66daff1e87619a67a4ef71bafc2abbfa65'
---
+++ test/unit/wolf_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class WolfTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
